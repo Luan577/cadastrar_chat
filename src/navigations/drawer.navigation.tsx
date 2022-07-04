d1391c069561e3addc85d1b5d66796fd39e7421d
Luan577 <dluan577@gmail.com>
2022-07-04 17:13:05 -0300

    tela de cadastrar para chat

---
+++ src/navigations/drawer.navigation.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { ChatScreen, PerfilScreen } from "../screens";
+import colors from "../styles/colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: { backgroundColor: colors.secondary },
+        headerTintColor: colors.white,
+        drawerStyle: { backgroundColor: colors.secondary },
+        drawerActiveTintColor: colors.white,
+        drawerInactiveTintColor: colors.white,
+      }}
+    >
+      <Drawer.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          drawerIcon: () => (
+            <Ionicons name="chatbubbles" size={24} color={colors.white} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
